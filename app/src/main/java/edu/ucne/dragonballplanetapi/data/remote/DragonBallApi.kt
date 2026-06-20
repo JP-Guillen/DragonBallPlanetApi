d035b0c0cc22d5fbf9bc2242a610bdf19935339e
JP-Guillen <juan_guillen@ucne.edu.do>
2026-06-20 15:21:09 -0400

    Crea el DragonBallApi

---
+++ app/src/main/java/edu/ucne/dragonballplanetapi/data/remote/DragonBallApi.kt
@@ -0,0 +1,27 @@
+package edu.ucne.dragonballplanetapi.data.remote
+
+import edu.ucne.dragonballplanetapi.data.remote.dto.PlanetDto
+import edu.ucne.dragonballplanetapi.data.remote.dto.PlanetResponseDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface DragonBallApi {
+
+    @GET("planets")
+    suspend fun getPlanets(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int,
+        @Query("name") name: String?
+    ): Response<PlanetResponseDto>
+
+    @GET("planets")
+    suspend fun searchPlanets(
+        @Query("name") name: String
+    ): Response<List<PlanetDto>>
+    @GET("planets/{id}")
+    suspend fun getPlanetDetail(
+        @Path("id") id: Int
+    ): Response<PlanetDto>
+}
