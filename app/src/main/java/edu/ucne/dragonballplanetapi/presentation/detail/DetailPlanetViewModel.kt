af7b67db4d6bd39cdf91389afca42c6d919f7cad
JP-Guillen <juan_guillen@ucne.edu.do>
2026-06-20 15:27:00 -0400

    Crea el DetailPlanetViewModel

---
+++ app/src/main/java/edu/ucne/dragonballplanetapi/presentation/detail/DetailPlanetViewModel.kt
@@ -0,0 +1,42 @@
+package edu.ucne.dragonballplanetapi.presentation.detail
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.dragonballplanetapi.data.remote.Resource
+import edu.ucne.dragonballplanetapi.domain.usecase.GetPlanetDetailUseCase
+import edu.ucne.dragonballplanetapi.presentation.navigation.Screen
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailPlanetViewModel @Inject constructor(
+    private val getPlanetDetailUseCase: GetPlanetDetailUseCase,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(DetailPlanetUiState())
+    val state = _state.asStateFlow()
+
+    init {
+        val args = savedStateHandle.toRoute<Screen.Detail>()
+        loadPlanet(args.id)
+    }
+
+    private fun loadPlanet(id: Int) {
+        viewModelScope.launch {
+            getPlanetDetailUseCase(id).collect { result ->
+                when (result) {
+                    is Resource.Loading -> _state.update { it.copy(isLoading = true) }
+                    is Resource.Success -> _state.update { it.copy(isLoading = false, planet = result.data) }
+                    is Resource.Error -> _state.update { it.copy(isLoading = false, error = result.message) }
+                }
+            }
+        }
+    }
+}
