de5294eda56cb71eaebeb9f8504a4c97b2696e1b
JP-Guillen <juan_guillen@ucne.edu.do>
2026-06-20 15:20:53 -0400

    Crea el GetPlanetlUseCase

---
+++ app/src/main/java/edu/ucne/dragonballplanetapi/domain/usecase/GetPlanetUseCase.kt
@@ -0,0 +1,14 @@
+package edu.ucne.dragonballplanetapi.domain.usecase
+
+import edu.ucne.dragonballplanetapi.domain.repository.PlanetRepository
+import javax.inject.Inject
+
+class GetPlanetsUseCase @Inject constructor(
+    private val repository: PlanetRepository
+) {
+    operator fun invoke(
+        page: Int = 1,
+        limit: Int = 10,
+        name: String? = null
+    ) = repository.getPlanets(page, limit, name)
+}
